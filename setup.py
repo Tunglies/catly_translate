e1e9aa383d6b24492c379a73684069da638888c6
CatNeverCodes <574469831@qq.com>
2022-05-20 22:53:01 +0800

    |^w^ >

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     url="https://github.com/CatNeverCodes/catly_translate",
     package_dir={"":"src"},
     packages=setuptools.find_packages(where="src"),
-    include_package_data=True,
+    # include_package_data=True,
     package_data={
         "": ["*.js"]
     },
